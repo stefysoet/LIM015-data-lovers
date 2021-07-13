b2e303c230cd41d662330776b9cd382449ce37d0
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-07-13 11:25:57 -0500

    Haciendo revisiones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import data from './data/pokemon/pokemon.js';
 import{ example}from './data.js';
 
+//asignar valores para iniciar
+// const pokeCard= document.querySelector('');
+// const pokeName= document.querySelector('');
+// const pokeImg= document.querySelector('');
+// const pokeId= document.querySelector('');
+// const pokeTypes= document.querySelector('');
+
 //eslint-disable-next-line no-console
 console.log(example, data);
 const buttonpokedex = document.getElementById("pokedex");
@@ -8,12 +15,15 @@ buttonpokedex.addEventListener("click" , function(){
     document.getElementById("data").innerHTML=data.pokemon.map(example).join(" ")
 })
 //funcion filter
-function filtrar(tipo){
-    const look = data.pokemon.filter(poke => poke.type.includes(tipo));
-    console.log(look)
-  }
-  filtrar("grass");
-const buttonfiltrador =document.getElementById("filtrador");
-buttonfiltrador.addEventListener("click", function(){
-    console.log("soyunboton");
-})
+// function filtrar(tipo){
+//     const look = data.pokemon.filter(poke => poke.type.includes(tipo));
+//     console.log(look)
+//   }
+//   filtrar("grass");
+// const buttonfiltrador =document.getElementById("filtrador");
+// buttonfiltrador.addEventListener("click", function(){
+//     console.log("soyunboton");
+// })
+// llamando al tipo
+const selectElement = document.querySelector('.tipo');
+selectElement.addEventListener()
