846e551471469a9f55b1712cfb0c9b22a3090bd9
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-07-19 19:13:56 -0500

    Probando ingresar datos

--- src/main.js
+++ src/main.js
@@ -1,46 +1,51 @@
-import {filtrar} from './data.js';
-import data from './data/pokemon/pokemon.js';
+import { filtrar } from "./data.js";
+import data from "./data/pokemon/pokemon.js";
 const pokeInfo = data.pokemon;
 
-function buscarPokemon(pokeInfo){
-    const mostrarPokemon= document.querySelector('#info');
-    //console.log(pokeInfo);
-    for (let i=0; i<pokeInfo.length; i++){
-        const pokeImage= pokeInfo[i].img;
-        const pokeNombre= pokeInfo[i].name;
-        const pokeNumero= pokeInfo[i].num;
-        const pokeTipo= pokeInfo[i].type;
-        let mostrarTipo;
-        
-            for (let j=0; j<pokeTipo.length; j++){
-             mostrarTipo=pokeTipo[j];
-         }
+function buscarPokemon(pokeInfo) {
+  const mostrarPokemon = document.querySelector("#info");
+  //console.log(pokeInfo);
+  for (let i = 0; i < pokeInfo.length; i++) {
+    const pokeImage = pokeInfo[i].img;
+    const pokeNombre = pokeInfo[i].name;
+    const pokeNumero = pokeInfo[i].num;
+    const pokeTipo = pokeInfo[i].type;
 
-        let contenedorPadre = document.createElement('div');
-            contenedorPadre.classList.add("cardpokemon")
-        //Hijo del contenedor principal
-            let numero = document.createElement('p');
-            numero.classList.add("pokemonnumero")
-        numero.textContent = '#'+ pokeNumero;
-        contenedorPadre.appendChild(numero);
-        
-        let imagen = document.createElement('img');
-            imagen.classList.add("pokemonimagen")
-        imagen.src = pokeImage;
-        contenedorPadre.appendChild(imagen);
+    let contenedorPadre = document.createElement("div");
+    contenedorPadre.classList.add("cardpokemon");
+    //Hijo del contenedor principal
+    let numero = document.createElement("p");
+    numero.classList.add("pokemonnumero");
+    numero.textContent = "#" + pokeNumero;
+    contenedorPadre.appendChild(numero);
 
-        let nombre = document.createElement('p');
-            nombre.classList.add("pokemonnombre")
-        nombre.textContent = pokeNombre;
-        contenedorPadre.appendChild(nombre);
+    let imagen = document.createElement("img");
+    imagen.classList.add("pokemonimagen");
+    imagen.src = pokeImage;
+    contenedorPadre.appendChild(imagen);
 
-        let tipo = document.createElement('p');
-            tipo.classList.add("pokemontipo")
-        tipo.textContent = mostrarTipo;
-        contenedorPadre.appendChild(tipo);
+    let nombre = document.createElement("p");
+    nombre.classList.add("pokemonnombre");
+    nombre.textContent = pokeNombre;
+    contenedorPadre.appendChild(nombre);
 
-        mostrarPokemon.appendChild(contenedorPadre);
-    }
+    let tipo = document.createElement("p");
+    tipo.classList.add("pokemontipo");
+    tipo.textContent = pokeTipo.join(" ");
+    contenedorPadre.appendChild(tipo);
+
+    let verMas = document.createElement("a");
+    verMas.classList.add("masInfo");
+    verMas.textContent = "Ver más";
+
+    verMas.addEventListener("click", (e) => {
+      e.preventDefault();
+      fichaTecnica(pokeInfo[i]);
+    });
+
+    nombre.appendChild(verMas);
+    mostrarPokemon.appendChild(contenedorPadre);
+  }
 }
 // const todosPokemon = pokeInfo.map(pokemon => {
 //     return pokemon;
@@ -51,16 +56,16 @@ function buscarPokemon(pokeInfo){
 
 buscarPokemon(pokeInfo);
 
-const filtro = document.querySelector('#tipo');
+const filtro = document.querySelector("#tipo");
 filtro.addEventListener("change", (event) => {
-    const tipoSelec = event.target.value;
-    const resultado = filtrar(tipoSelec,pokeInfo);
-    limpiarPokemon();
-    buscarPokemon(resultado);
-} )
-//limpiar 
-function limpiarPokemon(){
-    document.querySelector('#info').innerHTML="";
+  const tipoSelec = event.target.value;
+  const resultado = filtrar(tipoSelec, pokeInfo);
+  limpiarPokemon();
+  buscarPokemon(resultado);
+});
+//limpiar
+function limpiarPokemon() {
+  document.querySelector("#info").innerHTML = "";
 }
 
 //Probando busqueda
@@ -77,20 +82,32 @@ function limpiarPokemon(){
 
 // }
 // botonSearch.addEventListener('click',insertPokemon);
-//botonSearch.addEventListener('touchstart',insertPokemon); //para dispositivos moviles 
+//botonSearch.addEventListener('touchstart',insertPokemon); //para dispositivos moviles
 
-function buscadorPokemon(){
-    let ingresarDato = document.querySelector('#pokeName').value;
-    let guardarInfo;
-     if(isNaN(ingresarDato)){
-       guardarInfo = pokeInfo.filter((poke) => poke.name.includes(ingresarDato));
-     } 
-     else{
-       guardarInfo = pokeInfo.filter((poke) => parseInt(poke.num) == parseInt(ingresarDato));
-     }
-     limpiarPokemon();
-     buscarPokemon(guardarInfo);
+function buscadorPokemon() {
+  let ingresarDato = document.querySelector("#pokeName").value;
+  let guardarInfo;
+  if (isNaN(ingresarDato)) {
+    guardarInfo = pokeInfo.filter((poke) => poke.name.includes(ingresarDato));
+  } else {
+    guardarInfo = pokeInfo.filter(
+      (poke) => parseInt(poke.num) == parseInt(ingresarDato)
+    );
   }
+  limpiarPokemon();
+  buscarPokemon(guardarInfo);
+}
 
-document.querySelector('#searchPokemon').addEventListener('click',function(){ buscadorPokemon();
+document.querySelector("#searchPokemon").addEventListener("click", function () {
+  buscadorPokemon();
 });
+
+function fichaTecnica(pokeInfo) {
+  document.querySelector("#info").style.display = "none";
+  document.querySelector("#contenedorFicha").style.display = "block";
+  // limpiarPokemon("#contenedorFicha");
+  //console.log(pokeInfo);
+  const fichaPoke = document.querySelector("#contenedorFicha");
+  fichaPoke.innerHTML = `<section><p>${pokeInfo.name}</p><img src="${pokeInfo.img}">
+  </section>`;
+}
