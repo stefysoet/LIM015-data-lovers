4b3139c91f953dcafdaf858d822fab6b6663f4be
stefysoet <stefysoet@hotmail.com>
2021-07-22 17:16:08 -0500

    style-card

--- src/data.js
+++ src/data.js
@@ -2,17 +2,14 @@
 export function filtrar(tipoSelec, pokeInfo) {
   return pokeInfo.filter((poke) => poke.type.includes(tipoSelec));
 }
-
 export function verRegion(region, pokeInfo) {
   const datoP = pokeInfo.filter((pokemon) => pokemon.generation.name == region);
   return datoP;
 }
-
 // export function accederObject(stats, pokeInfo) {
 //   const pokeObject = pokeInfo.map((pokestats) => pokestats.stats);
 //   return pokeObject;
 // }
-
 export function ordenaNombre(ab, pokeInfo) {
   let ordenarN = [];
   if (ab == "ascendente") {
