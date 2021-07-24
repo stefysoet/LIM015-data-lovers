eabc19ab5c05ba09ee6f5817b222fcd14022f0f2
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-07-24 12:09:17 -0500

    improvisando test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,13 @@
 // import { example, anotherExample } from '../src/data.js';
 
-import { ordenaNombre } from '../src/data.js';
+//import { ordenaNombre } from '../src/data.js';
 
-import data from '../src/data/pokemon/pokemon.js';
+//import data from '../src/data/pokemon/pokemon.js';
+//probando
+//expect().toBe('function');
+
+//it('returns ´´', () => {
+//   it(' ´´ ', () => {
+//     expect().toBe('');
+//   });
+// });
