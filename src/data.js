0952e842df382a4dadf63113ee6192240281e830
stefysoet <stefysoet@hotmail.com>
2021-07-26 13:08:53 -0500

    Realizando test 3

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 //funcion filter
 export function filtrar(tipoSelec, pokeInfo) {
+  console.log (tipoSelec);
   return pokeInfo.filter((poke) => poke.type.includes(tipoSelec));
 }
 export function verRegion(region, pokeInfo) {
