9c2196de336aafb437aae0a2a6c23eb05132f24d
EstefaniConislla <estefaniyulianaca@gmail.com>
2021-07-27 00:39:46 -0500

    No salieron los test

--- src/main.js
+++ src/main.js
@@ -14,42 +14,42 @@ addEventListener("DOMContentLoaded", () => {
 });
 
 // ------------------- BARRA DE NAVEGACIÓN---------------------
-const nav = document.querySelectorAll('.container li');
-document.getElementById('inicio').addEventListener('click', function() { 
-    for(let link of nav){
-        link.classList.remove('linkActivo');
-      }
-      document.getElementById('inicio').classList.add('linkActivo');
-      displayMain('inicio');
+const nav = document.querySelectorAll(".container li");
+document.getElementById("inicio").addEventListener("click", function () {
+  for (let link of nav) {
+    link.classList.remove("linkActivo");
+  }
+  document.getElementById("inicio").classList.add("linkActivo");
+  displayMain("inicio");
 });
-document.getElementById('pokedex').addEventListener('click', function() { 
-    for(let link of nav){
-        link.classList.remove('linkActivo');
-      }
-      document.getElementById('pokedex').classList.add('linkActivo');
-      displayMain('pokedex');
+document.getElementById("pokedex").addEventListener("click", function () {
+  for (let link of nav) {
+    link.classList.remove("linkActivo");
+  }
+  document.getElementById("pokedex").classList.add("linkActivo");
+  displayMain("pokedex");
 });
-document.getElementById('usuarios').addEventListener('click', function() { 
-  for(let link of nav){
-      link.classList.remove('linkActivo');
-    }
-    document.getElementById('usuarios').classList.add('linkActivo');
-    displayMain('usuarios');
+document.getElementById("usuarios").addEventListener("click", function () {
+  for (let link of nav) {
+    link.classList.remove("linkActivo");
+  }
+  document.getElementById("usuarios").classList.add("linkActivo");
+  displayMain("usuarios");
 });
 const displayMain = (id) => {
-    if (id=='inicio') {
-      document.getElementById('nav_inicio').style.display= 'block';
-      document.getElementById('nav_pokedex').style.display = 'none';
-      document.getElementById('nav_usuarios').style.display= 'none';
-    } else if (id == 'pokedex') {
-      document.getElementById('nav_inicio').style.display= 'none';
-      document.getElementById('nav_pokedex').style.display = 'block';
-      document.getElementById('nav_usuarios').style.display= 'none';
-    } else if (id == 'usuarios'){
-      document.getElementById('nav_inicio').style.display= 'none';
-      document.getElementById('nav_pokedex').style.display = 'none';
-      document.getElementById('nav_usuarios').style.display= 'block';
-    } 
+  if (id == "inicio") {
+    document.getElementById("nav_inicio").style.display = "block";
+    document.getElementById("nav_pokedex").style.display = "none";
+    document.getElementById("nav_usuarios").style.display = "none";
+  } else if (id == "pokedex") {
+    document.getElementById("nav_inicio").style.display = "none";
+    document.getElementById("nav_pokedex").style.display = "block";
+    document.getElementById("nav_usuarios").style.display = "none";
+  } else if (id == "usuarios") {
+    document.getElementById("nav_inicio").style.display = "none";
+    document.getElementById("nav_pokedex").style.display = "none";
+    document.getElementById("nav_usuarios").style.display = "block";
+  }
 };
 
 //POKÉX
@@ -135,11 +135,8 @@ region.addEventListener("change", (event) => {
   limpiarPokemon();
   buscarPokemon(generacionResultado);
 });
-// //Intentando obtener valor
-// sortValores.addEventListener(change, (e) => {
-//   let sortValores = e.currentTargert.value;
-//   console.log(sortValores);
-// });
+
+//funcion LIMPIAR
 function limpiarPokemon() {
   document.querySelector("#info").innerHTML = "";
 }
